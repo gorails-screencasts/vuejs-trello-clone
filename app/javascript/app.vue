478942e81798aeaee26ec7f18b3e8831ee2079d5
Chris Oliver <excid3@gmail.com>
2017-11-30 10:58:43 -0600

    Improve styling and UI interaction to be more like Trello

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,7 @@
 <template>
-  <draggable v-model="lists" :options="{group: 'lists'}" class="row dragArea" @end="listMoved">
-    <div v-for="(list, index) in lists" class="col-3">
+  <draggable v-model="lists" :options="{group: 'lists'}" class="board dragArea" @end="listMoved">
+    <div v-for="(list, index) in lists" class="list">
       <h6>{{ list.name }}</h6>
-      <hr />
 
       <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
         <div v-for="(card, index) in list.cards" class="card card-body mb-3">
@@ -10,10 +9,8 @@
         </div>
       </draggable>
 
-      <div class="card card-body">
-        <textarea v-model="messages[list.id]" class="form-control"></textarea>
-        <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
-      </div>
+      <textarea v-model="messages[list.id]" class="form-control mb-1"></textarea>
+      <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
     </div>
   </draggable>
 </template>
@@ -93,5 +90,20 @@ export default {
 <style scoped>
 .dragArea {
   min-height: 10px;
+}
+
+.board {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.list {
+  background: #E2E4E6;
+  border-radius: 3px;
+  display: inline-block;
+  margin-right: 20px;
+  padding: 10px;
+  vertical-align: top;
+  width: 270px;
 }
 </style>
