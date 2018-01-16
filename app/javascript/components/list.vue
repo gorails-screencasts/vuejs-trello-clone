94db5efbd25bbac3db39d8cd24e8c3213d0be154
Chris Oliver <excid3@gmail.com>
2018-01-15 18:54:34 -0600

    fix modal edit with key

--- app/javascript/components/list.vue
+++ app/javascript/components/list.vue
@@ -3,7 +3,7 @@
     <h6>{{ list.name }}</h6>
 
     <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
-      <card v-for="card in list.cards" :card="card" :list="list"></card>
+      <card v-for="card in list.cards" :key="card.id" :card="card" :list="list"></card>
     </draggable>
 
     <a v-if="!editing" v-on:click="startEditing">Add a card</a>
